6e515aee278726478db8792c131addf95efafb6b
Francesc Vila <francescvilasubias@gmail.com>
2024-06-15 19:58:06 +0200

    feat: prevent hydration missmatch when page is scrolled on load

--- components/aside/Right.vue
+++ components/aside/Right.vue
@@ -13,12 +13,14 @@ async function navigateToTop() {
 
 <template>
   <aside class="col-span-3">
-    <button
-      class="fixed bottom-6 right-8 rounded-full p-2 transition-opacity duration-500 hover:bg-gray-300/20"
-      :class="y > SCROLL_TO_TOP_OFFSET ? 'opacity-100' : 'cursor-default opacity-0'"
-      @click="navigateToTop"
-    >
-      <Icon name="ph:arrow-fat-line-up" size="24" />
-    </button>
+    <ClientOnly>
+      <button
+        class="fixed bottom-6 right-8 rounded-full p-2 transition-opacity duration-500 hover:bg-zinc-300/20"
+        :class="y > SCROLL_TO_TOP_OFFSET ? 'opacity-100' : 'cursor-default opacity-0'"
+        @click="navigateToTop"
+      >
+        <Icon name="ph:arrow-fat-line-up" size="24" />
+      </button>
+    </ClientOnly>
   </aside>
 </template>
