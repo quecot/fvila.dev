ca502457a124de4d82617f546626f9e6c9bea017
Francesc Vila <francescvilasubias@gmail.com>
2024-06-15 19:17:10 +0200

    feat: add sublinks to left sidebar

--- components/aside/Left.vue
+++ components/aside/Left.vue
@@ -11,6 +11,11 @@ const links = data.value?.body?.toc?.links ?? [];
     <ul class="fixed">
       <li v-for="link of links" :key="link.id">
         <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+        <ul class="ml-4">
+          <li v-for="sublink in link.children" :key="sublink.id">
+            <NuxtLink :to="`#${sublink.id}`">{{ sublink.text }}</NuxtLink>
+          </li>
+        </ul>
       </li>
     </ul>
   </aside>
