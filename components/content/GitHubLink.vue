764f1570af11d946f9f54e134484f36b06fa8393
Francesc Vila <francescvilasubias@gmail.com>
2024-06-16 21:55:37 +0200

    refactor: make github icon larger in links

--- components/content/GitHubLink.vue
+++ components/content/GitHubLink.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <a :href="href" target="_blank" class="w-fit">
     <code class="space-x-1 px-1 before:hidden after:hidden">
-      <Icon name="ph:github-logo" size="12" />
+      <Icon name="ph:github-logo" size="16" />
       <span class="!font-mono">{{ title }}</span>
     </code>
   </a>
