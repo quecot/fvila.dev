2ddf55decadf3a8a75ca24487397ec35e4a5c1c2
Francesc Vila <francescvilasubias@gmail.com>
2024-06-16 15:59:24 +0200

    fix: LinkCard min-w and spacing

--- components/content/LinkCard.vue
+++ components/content/LinkCard.vue
@@ -8,8 +8,8 @@ defineProps<{
   <div
     class="rounded-md border border-dashed border-zinc-300 bg-zinc-100 px-3 py-4 transition-colors duration-500 hover:border hover:border-solid dark:border-zinc-700 dark:bg-zinc-800"
   >
-    <a :href="href" class="flex items-center space-x-2 no-underline prose-p:m-0 prose-p:font-semibold">
-      <Icon name="ph:link" size="20" />
+    <a :href="href" class="flex items-center space-x-3 no-underline prose-p:m-0 prose-p:font-semibold">
+      <Icon name="ph:link" size="20" class="min-w-5" />
       <ContentSlot />
     </a>
   </div>
