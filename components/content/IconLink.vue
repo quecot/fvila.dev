d8b0dde4daefbe1c979ba36843e3fbc9f3fa591a
Francesc Vila <francescvilasubias@gmail.com>
2024-06-17 19:34:14 +0200

    refactor: abstract GitHubLink content component to more general IconLink component

--- components/content/IconLink.vue
+++ components/content/IconLink.vue
@@ -2,13 +2,14 @@
 defineProps<{
   title: string;
   href: string;
+  icon: string;
 }>();
 </script>
 
 <template>
   <a :href="href" target="_blank" class="w-fit">
     <code class="space-x-1 px-1 before:hidden after:hidden">
-      <Icon name="ph:github-logo" size="16" />
+      <Icon :name="icon" size="16" />
       <span class="!font-mono">{{ title }}</span>
     </code>
   </a>
