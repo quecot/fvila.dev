3ed7a883a236cd27831027127722f5b7ea99df91
Francesc Vila <francescvilasubias@gmail.com>
2024-06-17 19:37:59 +0200

    fix: LinkCard anchor area and target="_blank"

--- components/content/LinkCard.vue
+++ components/content/LinkCard.vue
@@ -5,12 +5,12 @@ defineProps<{
 </script>
 
 <template>
-  <div
-    class="rounded-md border border-dashed border-zinc-300 bg-zinc-100 px-3 py-4 transition-colors duration-500 hover:border hover:border-solid hover:border-zinc-500 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:border-zinc-400"
+  <a
+    :href="href"
+    target="_blank"
+    class="flex items-center space-x-3 text-pretty rounded-md border border-dashed border-zinc-300 bg-zinc-100 px-3 py-4 no-underline transition-colors duration-500 hover:border hover:border-solid hover:border-zinc-500 prose-p:m-0 prose-p:font-semibold dark:border-zinc-700 dark:bg-zinc-800 dark:hover:border-zinc-400"
   >
-    <a :href="href" class="flex items-center space-x-3 text-pretty no-underline prose-p:m-0 prose-p:font-semibold">
-      <Icon name="ph:link" size="20" class="min-w-5" />
-      <ContentSlot />
-    </a>
-  </div>
+    <Icon name="ph:link" size="20" class="min-w-5" />
+    <ContentSlot />
+  </a>
 </template>
