b75c0e4b09cbbb1f126aabacf89892044e1a92e3
Francesc Vila <francescvilasubias@gmail.com>
2024-06-22 15:54:51 +0200

    refactor: toc vertical spacing

--- components/aside/Left.vue
+++ components/aside/Left.vue
@@ -16,7 +16,7 @@ const links = data.value?.body?.toc?.links ?? [];
       />
 
       <ul class="opacity-0 transition-opacity duration-500 group-hover:opacity-100">
-        <li v-for="link of links" :key="link.id" class="space-y-0.5">
+        <li v-for="link of links" :key="link.id" class="space-y-1.5">
           <NuxtLink
             class="text-zinc-500 underline decoration-zinc-300 underline-offset-2 transition-colors duration-500 hover:text-zinc-900 hover:decoration-zinc-500 dark:decoration-zinc-700 dark:hover:text-zinc-300 dark:hover:decoration-zinc-500"
             :to="`#${link.id}`"
@@ -25,7 +25,7 @@ const links = data.value?.body?.toc?.links ?? [];
           </NuxtLink>
 
           <ul class="ml-4">
-            <li v-for="sublink in link.children" :key="sublink.id" class="space-y-0.5">
+            <li v-for="sublink in link.children" :key="sublink.id" class="-mt-0.5 mb-2 space-y-1">
               <NuxtLink
                 class="text-zinc-500 underline decoration-zinc-300 underline-offset-2 transition-colors duration-500 hover:text-zinc-900 hover:decoration-zinc-500 dark:decoration-zinc-700 dark:hover:text-zinc-300 dark:hover:decoration-zinc-500"
                 :to="`#${sublink.id}`"
